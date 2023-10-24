5fdefa4cf7120c147f31df1f5a18b35becdc06a7
Zachary Walton <zackwalton15@gmail.com>
2023-10-23 22:22:15 -0400

    Project page work

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -8,6 +8,10 @@ interface Props {
 }
 
 export default function Project({params}: Props) {
+  const slug = params.slug;
+
   return <div>
+    <a href={"/"} className={"font-bold font-eigerdals underline"}>Home</a><br/>
+    <a href={"/"} className={"text-5xl font-bold font-eigerdals colourful"}>{slug}</a>
   </div>
 }
